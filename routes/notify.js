a86bdc787389e4c8034976825d7e6f21f575bbc5
Søren Qvist Jensen <soerenq@gmail.com>
2017-04-01 23:04:38 +0200

    Work to servo control

--- routes/notify.js
+++ routes/notify.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var Gpio = require('pigpio').Gpio;
 var router = express.Router();
 
-/* GET users listing. */
+var motor = new Gpio(18, {mode: Gpio.OUTPUT}),
+    pulseWidth = 1000,
+    increment = 100;
+
+setInterval(function () {
+    motor.servoWrite(pulseWidth);
+
+    pulseWidth += increment;
+    if (pulseWidth >= 2000) {
+        increment = -100;
+    } else if (pulseWidth <= 1000) {
+        increment = 100;
+    }
+}, 1000);
 router.post('/', function(req, res, next) {
+  //TODO if some reward condition is hold
+
+
   res.sendStatus(200);
+
 });
 
 module.exports = router;
