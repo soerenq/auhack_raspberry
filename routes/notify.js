4423077178f2dc1e274af4ef3349f12396a9a699
Søren Qvist Jensen <soerenq@gmail.com>
2017-04-02 01:20:58 +0200

    Test

--- routes/notify.js
+++ routes/notify.js
@@ -3,14 +3,14 @@ var Gpio = require('pigpio').Gpio;
 var router = express.Router();
 
 var motor = new Gpio(18, {mode: Gpio.OUTPUT});
-var pulseWidth = 2000;
-var increment = 100;
+var pulseWidth = 500;
+var increment = 500;
 
 setInterval(function () {
     motor.servoWrite(pulseWidth);
 
 
-    pulseWidth += increment;
+//    pulseWidth += increment;
     /*if (pulseWidth >= 2000) {
         increment = -100;
     } else if (pulseWidth <= 1000) {
