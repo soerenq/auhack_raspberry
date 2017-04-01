af58465ad7c62bd206602d4ac1579a8f642d27c1
Søren Qvist Jensen <soerenq@gmail.com>
2017-04-02 00:17:29 +0200

    Test

--- routes/notify.js
+++ routes/notify.js
@@ -3,18 +3,18 @@ var Gpio = require('pigpio').Gpio;
 var router = express.Router();
 
 var motor = new Gpio(18, {mode: Gpio.OUTPUT});
-var pulseWidth = 500;
+var pulseWidth = 1200;
 var increment = 50;
 
 setInterval(function () {
     motor.servoWrite(pulseWidth);
 
-    pulseWidth += increment;
-    if (pulseWidth >= 1000) {
+    /*pulseWidth += increment;
+    if (pulseWidth >= 2000) {
         increment = -50;
-    } else if (pulseWidth <= 500) {
+    } else if (pulseWidth <= 1000) {
         increment = 50;
-    }
+    }*/
 }, 1000);
 router.post('/', function(req, res, next) {
   //TODO if some reward condition is hold
