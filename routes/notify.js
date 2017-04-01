8db96472923fd83c9c8d2da0aed40b1d80a84bb7
Søren Qvist Jensen <soerenq@gmail.com>
2017-04-02 01:52:23 +0200

    Cleanup in notify

--- routes/notify.js
+++ routes/notify.js
@@ -1,16 +1,6 @@
 var express = require('express');
-var Gpio = require('pigpio').Gpio;
 var router = express.Router();
 
-var motor = new Gpio(18, {mode: Gpio.OUTPUT});
-var openPos = 1500;
-var closedPos = 525;
-
-var prev = openPos;
-setInterval(function () {
-    prev = prev === openPos ? closedPos : openPos;
-    motor.servoWrite(prev);
-}, 1000);
 router.post('/', function(req, res, next) {
   //TODO if some reward condition is hold
 
