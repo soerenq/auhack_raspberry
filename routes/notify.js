d02090363f19d82ae818087047b54bd9f4b61082
Søren Qvist Jensen <soerenq@gmail.com>
2017-04-01 23:44:58 +0200

    Tried other values for PWM

--- routes/notify.js
+++ routes/notify.js
@@ -2,18 +2,18 @@ var express = require('express');
 var Gpio = require('pigpio').Gpio;
 var router = express.Router();
 
-var motor = new Gpio(18, {mode: Gpio.OUTPUT}),
-    pulseWidth = 1000,
-    increment = 100;
+var motor = new Gpio(18, {mode: Gpio.OUTPUT});
+var pulseWidth = 1000;
+var increment = 250;
 
 setInterval(function () {
     motor.servoWrite(pulseWidth);
 
     pulseWidth += increment;
     if (pulseWidth >= 2000) {
-        increment = -100;
+        increment = -250;
     } else if (pulseWidth <= 1000) {
-        increment = 100;
+        increment = 250;
     }
 }, 1000);
 router.post('/', function(req, res, next) {
